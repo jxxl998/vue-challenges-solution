fe57aad19f8b520ad08681e8cbd3626ce163afd6
jxxl998 <jessielee1998_sc@foxmail.com>
2022-11-24 12:29:10 +0800

    easy: lifecycle hooks

---
+++ easy/lifecycle-hooks.vue
@@ -0,0 +1,30 @@
+// Child.vue
+
+<script setup>
+/**
+ * For this challenge, you'll use the Composition API: Lifecycle Hooks to complete the challenge.
+ *
+ * 组件卸载时清除计时器即可
+ */
+import { onMounted, inject, onUnmounted } from 'vue';
+
+const timer = inject('timer');
+const count = inject('count');
+
+onMounted(() => {
+  // The timer will work abnormally when the child component is toggled. Lets fix it.
+  timer.value = window.setInterval(() => {
+    count.value++;
+  }, 1000);
+});
+
+onUnmounted(() => {
+  clearInterval(timer.value);
+});
+</script>
+
+<template>
+  <div>
+    <p>Child Component: {{ count }}</p>
+  </div>
+</template>
