333c105f3fec6c2d7207c42676862b6a2a9a6914
jxxl998 <jessielee1998_sc@foxmail.com>
2022-11-28 20:25:00 +0800

    complete dom portal by Teleport feature

--- src/challenges/built-ins/dom-portal.vue
+++ src/challenges/built-ins/dom-portal.vue
@@ -2,11 +2,12 @@
 /**
  * Vue.js provides a built-in component that renders its slot content to another part of the DOM.
  */
-const msg = "Hello World"
-
+const msg = 'Hello World';
 </script>
 
 <template>
   <!-- Renders it to a child element of the `body` -->
-  <span>{{ msg }}</span>
+  <Teleport to="body">
+    <span>{{ msg }}</span>
+  </Teleport>
 </template>
