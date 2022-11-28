d20dd45737aa8db75ac7ef441830fa1a07b8f4ad
jxxl998 <jessielee1998_sc@foxmail.com>
2022-11-28 22:12:43 +0800

    complete functional component

--- src/challenges/components/functional-component.vue
+++ src/challenges/components/functional-component.vue
@@ -1,12 +1,84 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { h, ref } from 'vue';
 
 /**
  * Implement a functional component :
  * 1. Render the list elements (ul/li) with the list data
  * 2. Change the list item text color to red when clicked.
+ *
+ * solution: https://vuejs.org/guide/extras/render-function.html#functional-components
  */
-const ListComponent = () => {};
+const ListComponent = (props, { slots, emit, attrs }) => {
+  console.log('props', props);
+
+  const { list, activeIndex } = props;
+
+  const listVNodes = list.map((item, index) => {
+    return h(
+      'li',
+      {
+        key: index,
+        style: {
+          color: index === activeIndex ? 'red' : null,
+        },
+        onClick: () => emit('toggle', index),
+      },
+      item.name
+    );
+  });
+
+  return h(
+    'ul',
+    {
+      style: {
+        listStyleType: 'none',
+      },
+    },
+    listVNodes
+  );
+};
+
+// key point!!
+// If the props option is not specified, then the props object passed to the function will contain all attributes, the same as attrs.
+// The prop names will not be normalized to camelCase unless the props option is specified.
+
+// before
+/**
+ * {
+    "list": [
+        {
+            "name": "John"
+        },
+        {
+            "name": "Doe"
+        },
+        {
+            "name": "Smith"
+        }
+    ],
+    "active-index": 0
+}
+ */
+
+ListComponent.props = ['list', 'activeIndex'];
+
+// after
+/**
+ * {
+    "list": [
+        {
+            "name": "John"
+        },
+        {
+            "name": "Doe"
+        },
+        {
+            "name": "Smith"
+        }
+    ],
+    "activeIndex": 0
+}
+ */
 
 const list = [
   {
--- src/challenges/index.vue
+++ src/challenges/index.vue
@@ -1,14 +1,19 @@
 <script setup>
-import dynamicCssValuesVue from './css-features/dynamic-css-values.vue';
-import globalCssVue from './css-features/global-css.vue';
+// import dynamicCssValuesVue from './css-features/dynamic-css-values.vue';
+// import globalCssVue from './css-features/global-css.vue';
+
+import functionalComponentVue from './components/functional-component.vue';
 </script>
 
 <template>
   <!-- <h3>dynamic-css-values</h3>
   <dynamicCssValuesVue /> -->
 
-  <h3>global-css</h3>
-  <globalCssVue />
+  <!-- <h3>global-css</h3>
+  <globalCssVue /> -->
+
+  <h3>functional-component</h3>
+  <functionalComponentVue />
 </template>
 
 <style scoped>
