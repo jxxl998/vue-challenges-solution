510b2a58a62ee1e465eea3424284b4a7dd792c44
jxxl998 <jessielee1998_sc@foxmail.com>
2022-11-28 20:57:23 +0800

    custom validator function

--- src/challenges/components/prop-validation.vue
+++ src/challenges/components/prop-validation.vue
@@ -3,9 +3,20 @@
  * Please validate the type prop of the Button component.
  *
  * It's accept the following strings primary | ghost | dashed | link | text | default only and the default value is default.
+ *
+ * solution: https://cn.vuejs.org/guide/components/props.html#prop-validation
  */
 defineProps({
-  type: {},
+  type: {
+    type: String,
+    // 自定义类型校验函数
+    validator(value) {
+      return ['primary', 'ghost', 'dashed', 'link', 'text', 'default'].includes(
+        value
+      );
+    },
+    default: 'default',
+  },
 });
 </script>
 
