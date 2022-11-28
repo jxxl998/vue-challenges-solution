427e68ffa89999e6528b0f5c99486ff374394e83
jxxl998 <jessielee1998_sc@foxmail.com>
2022-11-28 20:35:21 +0800

    init

---
+++ src/challenges/css-features/global-css.vue
@@ -0,0 +1,25 @@
+<script setup>
+/**
+ * Sometimes, we may want to set global CSS in the scoped component.
+ */
+</script>
+
+<template>
+  <p>Hello Vue.js</p>
+</template>
+
+<style scoped>
+p {
+  font-size: 20px;
+  color: red;
+  text-align: center;
+  line-height: 50px;
+}
+
+/* Make it work */
+body {
+  width: 100vw;
+  height: 100vh;
+  background-color: burlywood;
+}
+</style>
