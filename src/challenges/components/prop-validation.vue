00ccfbba4a9650c471295eeabace74c43f89d9ca
jxxl998 <jessielee1998_sc@foxmail.com>
2022-11-28 20:53:33 +0800

    init

---
+++ src/challenges/components/prop-validation.vue
@@ -0,0 +1,14 @@
+<script setup>
+/**
+ * Please validate the type prop of the Button component.
+ *
+ * It's accept the following strings primary | ghost | dashed | link | text | default only and the default value is default.
+ */
+defineProps({
+  type: {},
+});
+</script>
+
+<template>
+  <button>Button</button>
+</template>
