66240fd15f96aa740bc5269a04aaafa9ec01bab0
jxxl998 <jessielee1998_sc@foxmail.com>
2022-11-28 20:21:23 +0800

    init dom portal

---
+++ src/challenges/built-ins/dom-portal.vue
@@ -0,0 +1,12 @@
+<script setup>
+/**
+ * Vue.js provides a built-in component that renders its slot content to another part of the DOM.
+ */
+const msg = "Hello World"
+
+</script>
+
+<template>
+  <!-- Renders it to a child element of the `body` -->
+  <span>{{ msg }}</span>
+</template>
