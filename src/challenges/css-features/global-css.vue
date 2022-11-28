03c01dc10a5b120c1f67cf6b53d9b8624a602e9f
jxxl998 <jessielee1998_sc@foxmail.com>
2022-11-28 20:50:06 +0800

    complete global css

--- src/challenges/css-features/global-css.vue
+++ src/challenges/css-features/global-css.vue
@@ -1,6 +1,12 @@
 <script setup>
 /**
  * Sometimes, we may want to set global CSS in the scoped component.
+ *
+ * https://cn.vuejs.org/api/sfc-css-features.html#scoped-css
+ *
+ * 使用全局选择器
+ *
+ * :global 伪类
  */
 </script>
 
@@ -17,6 +23,18 @@ p {
 }
 
 /* Make it work */
+/* 1 */
+/* 使用伪类 */
+:global(body) {
+  width: 100vw;
+  height: 100vh;
+  background-color: burlywood;
+}
+</style>
+
+<style>
+/* 2 */
+/* 全局样式 */
 body {
   width: 100vw;
   height: 100vh;
--- src/challenges/index.vue
+++ src/challenges/index.vue
@@ -1,10 +1,14 @@
 <script setup>
 import dynamicCssValuesVue from './css-features/dynamic-css-values.vue';
+import globalCssVue from './css-features/global-css.vue';
 </script>
 
 <template>
-  <h3>dynamic-css-values</h3>
-  <dynamicCssValuesVue />
+  <!-- <h3>dynamic-css-values</h3>
+  <dynamicCssValuesVue /> -->
+
+  <h3>global-css</h3>
+  <globalCssVue />
 </template>
 
 <style scoped>
