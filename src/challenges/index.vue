fe7c2c8f0b41384fe3be83e847fb369d5fb70b59
jxxl998 <jessielee1998_sc@foxmail.com>
2022-11-28 20:34:19 +0800

    complete dynamic css value by sfc features: v-bind directive

---
+++ src/challenges/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import dynamicCssValuesVue from './css-features/dynamic-css-values.vue';
+</script>
+
+<template>
+  <h3>dynamic-css-values</h3>
+  <dynamicCssValuesVue />
+</template>
+
+<style scoped>
+h3 {
+  color: salmon;
+}
+</style>
