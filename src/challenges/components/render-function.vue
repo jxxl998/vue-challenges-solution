6ce7858102e0e9e0bde6d06ac7ce4d755422ff71
jxxl998 <jessielee1998_sc@foxmail.com>
2022-11-28 23:44:19 +0800

    init

---
+++ src/challenges/components/render-function.vue
@@ -0,0 +1,18 @@
+<script setup>
+/**
+ * For this challenge, you need use h render function to implement a component.
+ *
+ * Note:
+ * You should make sure that the props are passed correctly,
+ * event is triggered correctly and the slot content is rendered correctly.
+ */
+import MyButton from './MyButton.js';
+
+const onClick = () => {
+  console.log('onClick');
+};
+</script>
+
+<template>
+  <MyButton :disabled="false" @custom-click="onClick"> my button </MyButton>
+</template>
